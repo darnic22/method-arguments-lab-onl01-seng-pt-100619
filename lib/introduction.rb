ce54ac910aafdd49010fcaf72784acb83f0c9f22
Nicole M Darby <nicoledarby021322@gmail.com>
2019-10-10 14:55:52 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,6 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
-def introduction(name,language)
+def introduction(Dan, Ember)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
